dec1299c89a8006d7ddfc47b77f99b1ac68eaca3
Martin Procházka <juniwalk@outlook.cz>
2023-01-06 13:57:16 +0100

    Added schema filtering class

---
+++ src/Utils/SchemaAssetsFilter.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+/**
+ * @copyright Martin Procházka (c) 2023
+ * @license   MIT License
+ */
+
+namespace JuniWalk\ORM\Utils;
+
+use Doctrine\DBAL\Schema\AbstractAsset;
+use Nette\Utils\Strings;
+
+final class SchemaAssetsFilter
+{
+	private array $assets;
+
+	public function __construct(
+		string ...$assets
+	) {
+		$this->assets = $assets;
+	}
+
+
+	public function __invoke(string|AbstractAsset $assetName): bool
+	{
+		if (empty($this->assets)) {
+			return true;
+		}
+
+		if ($assetName instanceof AbstractAsset) {
+			$assetName = $assetName->getName();
+		}
+
+		if (in_array($assetName, $this->assets)) {
+			return false;
+		}
+
+		return !Strings::match($assetName, '/'.implode('|', $this->assets).'/i');
+	}
+}
