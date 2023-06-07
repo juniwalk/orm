18849fc01fae5b6688fd8d6f5fe1fd67322ce921
Martin Procházka <juniwalk@outlook.cz>
2023-06-07 15:00:07 +0200

    Added DQL extension for to_tsquery function

---
+++ src/Functions/Query.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+/**
+ * @copyright Martin Procházka (c) 2023
+ * @license   MIT License
+ */
+
+namespace JuniWalk\ORM\Functions;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\AST\Node;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+/**
+ * "query" "(" Column, Query, Lang ")"
+ */
+final class Query extends FunctionNode
+{
+	public Node $column;
+	public Node $query;
+	public Node $lang;
+
+
+	public function parse(Parser $parser): void
+	{
+		$parser->match(Lexer::T_IDENTIFIER); // (2)
+		$parser->match(Lexer::T_OPEN_PARENTHESIS); // (3)
+
+		$this->column = $parser->StringPrimary(); // (4)
+
+		$parser->match(Lexer::T_COMMA); // (5)
+
+		$this->query = $parser->InstanceOfParameter(); // (6)
+
+		$parser->match(Lexer::T_COMMA); // (7)
+
+		$this->lang = $parser->StringPrimary(); // (8)
+
+		$parser->match(Lexer::T_CLOSE_PARENTHESIS); // (9)
+	}
+
+
+	public function getSql(SqlWalker $sqlWalker): string
+	{
+		$column = $sqlWalker->walkSimpleArithmeticExpression($this->column);
+		$query = $sqlWalker->walkSimpleArithmeticExpression($this->query);
+		$lang = $sqlWalker->walkSimpleArithmeticExpression($this->lang);
+
+		return "{$column} @@ to_tsquery({$lang}, {$query})";
+	}
+}
