4c5dc7ebfb6e268de7c065b88a10ab23beef4a66
Martin Procházka <juniwalk@outlook.cz>
2023-10-26 10:15:38 +0200

    Added traits for Authorship and Ownership

---
+++ src/Traits/Authorable.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+/**
+ * @copyright Martin Procházka (c) 2023
+ * @license   MIT License
+ */
+
+ namespace JuniWalk\ORM\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+use Nette\Security\IIdentity as Identity;
+
+trait Authorable
+{
+	#[ORM\ManyToOne(targetEntity: Identity::class)]
+	private ?Identity $author = null;
+
+
+	public function setAuthor(?Identity $author): void
+	{
+		$this->author = $author;
+	}
+
+
+	public function getAuthor(): ?Identity
+	{
+		return $this->author;
+	}
+
+
+	public function isAuthor(?Identity $author): bool
+	{
+		if (!$author XOR !$this->author) {
+			return false;
+		}
+
+		if (!$author && !$this->author) {
+			return true;
+		}
+
+		return $this->author->getId() === $author->getId();
+	}
+}
---
+++ src/Traits/Ownership.php
@@ -0,0 +1,37 @@
+
+<?php declare(strict_types=1);
+
+/**
+ * @copyright Martin Procházka (c) 2023
+ * @license   MIT License
+ */
+
+namespace App\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+use Nette\Security\IIdentity as Identity;
+
+trait Ownership
+{
+	#[ORM\ManyToOne(targetEntity: Identity::class)]
+	#[ORM\JoinColumn(nullable: false)]
+	private Identity $owner;
+
+
+	public function setOwner(Identity $owner): void
+	{
+		$this->owner = $owner;
+	}
+
+
+	public function getOwner(): Identity
+	{
+		return $this->owner;
+	}
+
+
+	public function isOwner(Identity $owner): bool
+	{
+		return $this->owner->getId() === $owner->getId();
+	}
+}
