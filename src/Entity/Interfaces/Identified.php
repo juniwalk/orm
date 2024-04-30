570adc2e3cf7ce888991ee86556b0849b2aff77f
Martin Procházka <juniwalk@outlook.cz>
2024-04-30 15:07:37 +0200

    Allow more types as Identifier

--- src/Entity/Interfaces/Identified.php
+++ src/Entity/Interfaces/Identified.php
@@ -7,8 +7,10 @@
 
 namespace JuniWalk\ORM\Entity\Interfaces;
 
+use Ramsey\Uuid\UuidInterface as Uuid;
+
 interface Identified
 {
-	public function getId(): ?int;
+	public function getId(): Uuid|int|string|null;
 	public function isPersisted(): bool;
 }
